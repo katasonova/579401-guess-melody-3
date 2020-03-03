017e86f167e5abefe0199bfd37a4bfa50d501fb9
Ekata <yogaelen108@gmail.com>
2020-03-03 23:50:36 +0000

    implemented initial setup for the router

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,12 +1,27 @@
 import React from "react";
 import WelcomeScreen from "../welcome-screen/welcome-screen.jsx";
 import PropTypes from 'prop-types';
+import {BrowserRouter, Route, Switch} from 'react-router-dom';
+import ArtistQuestionScreen from '../question-artist-screen/question-artist-screen.jsx';
+import GenreQuestionScreen from '../question-genre-screen/question-genre-screen.jsx';
 
 const welcomeButtonHandler = () => {};
 
 const App = (props) => {
   return (
-    <WelcomeScreen errors={props.errors} onWelcomeButtonClick={welcomeButtonHandler} />
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/">
+          <WelcomeScreen errors={props.errors} onWelcomeButtonClick={welcomeButtonHandler} />
+        </Route>
+        <Route exact path="/dev-artist">
+          <ArtistQuestionScreen />
+        </Route>
+        <Route exact path="/dev-genre">
+          <GenreQuestionScreen />
+        </Route>
+      </Switch>
+    </BrowserRouter>
   );
 };
 
@@ -15,4 +30,3 @@ App.propTypes = {
 };
 
 export default App;
-
